083e540a268eb5f89d47dbed839c8b6ec0b0f42d
Igone van_der_Kley <igone@vdkley.nl>
2023-06-02 11:40:54 +0200

    altered view

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,5 +1,5 @@
 <body>
     <h3><?= $data['title']; ?></h3>
-    <a href="<?= URLROOT; ?>/leerling/index">Overzicht Rijlespakketten Leerlingen</a><br>
+    <a href="<?= URLROOT; ?>/leerling/index">Overzicht Afgenomen Exames Examinatoren</a><br>
 
 </body>
