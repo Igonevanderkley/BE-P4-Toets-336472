36da6b558287f2e337f94dadc25014e1ebfa8ad7
Igone van_der_Kley <igone@vdkley.nl>
2023-06-02 12:15:45 +0200

    home link modified

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,5 +1,5 @@
 <body>
     <h3><?= $data['title']; ?></h3>
-    <a href="<?= URLROOT; ?>/leerling/index">Overzicht Afgenomen Exames Examinatoren</a><br>
+    <a href="<?= URLROOT; ?>/Examen/index">Overzicht Afgenomen Exames Examinatoren</a><br>
 
 </body>
